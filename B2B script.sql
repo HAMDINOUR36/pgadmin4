76e5022ace3ffb9aaa89c018eb89c6f40180d685
HAMDINOUR36 <100681145+HAMDINOUR36@users.noreply.github.com>
2024-04-19 10:35:18 +0100

    Add files via upload

---
+++ B2B script.sql
@@ -0,0 +1,14 @@
+-- Database: B2B
+
+-- DROP DATABASE IF EXISTS "B2B";
+
+CREATE DATABASE "B2B"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'French_Tunisia.1252'
+    LC_CTYPE = 'French_Tunisia.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
